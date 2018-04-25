ca575340ff1a30c146d4697e9c37bc032968f4dc
General <general@sujan.local>
2018-04-26 03:00:02 +0600

    defining label

---
+++ PicTaggerImagga/TagData.swift
@@ -0,0 +1,14 @@
+//
+//  TagData.swift
+//  PicTaggerImagga
+//
+//  Created by General on 4/25/18.
+//  Copyright © 2018 General. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct TagData {
+    var label: String
+}
